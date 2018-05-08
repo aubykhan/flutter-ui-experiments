c449a56de4cd73efec8b367019e0bd3b4a729d05
Asadullah Bin Yousuf <Asadullah.Yousuf@tpsonline.com>
2018-05-08 17:15:36 +0500

    Remove stack

--- lib/google_tasks_bottom_bar.dart
+++ lib/google_tasks_bottom_bar.dart
@@ -5,36 +5,29 @@ class GoogleTasksBottomAppBarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(title: const Text('Tasks - Bottom App Bar')),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 4.0,
+        icon: const Icon(Icons.add),
+        label: const Text('Add a task'),
+        onPressed: () {},
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: new Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          BottomAppBar(
-            hasNotch: true,
-            child: new Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          ),
-          new Padding(
-            padding: const EdgeInsets.only(bottom: 24.0),
-            child: FloatingActionButton.extended(
-              icon: const Icon(Icons.add),
-              label: const Text('Add a task'),
+      bottomNavigationBar: BottomAppBar(
+        hasNotch: false,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
               onPressed: () {},
             ),
-          ),
-        ],
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
